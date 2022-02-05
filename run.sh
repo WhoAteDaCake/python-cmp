14e16582662194b0aa47c2cf93aaf3a93388136c
WhoAteDaCake <doitsimplefy@gmail.com>
2022-02-05 10:20:10 +0000

    cleanup executables

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 odin-nightly build \
     src/main.odin \
     -collection:packages=packages \
-    -extra-linker-flags:"$(python3.9-config --ldflags)"
+    -extra-linker-flags:"$(python3.9-config --ldflags)" \
+    -out:dist/main
